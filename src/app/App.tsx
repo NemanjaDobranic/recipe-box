a3ce70e3d86d329aa80c34767d72df2495210564
nemanja <dobranicnemanja2014@gmail.com>
2026-02-19 20:31:31 +0100

    app page with routes

---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import '../App.css'
+import AppRoutes from "./routes.tsx";
+
+function App() {
+    return <AppRoutes />;
+}
+
+export default App
---
+++ src/app/routes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "@/pages/HomePage";
+import RecipeDetailsPage from "@/pages/RecipeDetailsPage";
+// import CreateRecipePage from "@/pages/CreateRecipePage";
+
+export default function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/recipe/:id" element={<RecipeDetailsPage />} />
+                {/*<Route path="/create" element={<CreateRecipePage />} />*/}
+            </Routes>
+        </BrowserRouter>
+    );
+}
