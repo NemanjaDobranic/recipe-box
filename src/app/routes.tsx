b7bbca4f22fab89e0450e20bf0ccc2456edc2714
nemanja <dobranicnemanja2014@gmail.com>
2026-02-21 13:34:12 +0100

    added shopping list page route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,6 +3,7 @@ import HomePage from "@/pages/HomePage";
 import RecipeDetailsPage from "@/pages/RecipeDetailsPage";
 import CreateRecipePage from "@/pages/CreateRecipePage";
 import EditRecipePage from "@/pages/EditRecipePage.tsx";
+import ShoppingListPage from "@/pages/ShopingListPage.tsx";
 
 export default function AppRoutes() {
     return (
@@ -12,6 +13,7 @@ export default function AppRoutes() {
                 <Route path="/recipe/:id" element={<RecipeDetailsPage />} />
                 <Route path="/create" element={<CreateRecipePage />} />
                 <Route path="/edit/:id" element={<EditRecipePage />} />
+                <Route path="shopping-list" element={<ShoppingListPage />} />
             </Routes>
         </BrowserRouter>
     );
