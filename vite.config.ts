a685ed09118390f07931ff53753134d2ca0cddab
nemanja <dobranicnemanja2014@gmail.com>
2026-02-23 21:24:34 +0100

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import * as path from "node:path";
 
+const isGhPages = process.env.GH_PAGES === "true";
 const repoName = "recipe-box";
 
 export default defineConfig({
-    base: `/${repoName}/`,
+    base: isGhPages ? `/${repoName}/` : "/",
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
